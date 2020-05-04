93f7c2de4faa0e0a05fb97a52365f5b611139e1a
Derkades <robinslot@hotmail.nl>
2020-05-05 00:26:02 +0200

    Just build, don't push

--- build-arm.sh
+++ build-arm.sh
@@ -1,2 +1,1 @@
 docker build -t derkades/rclone-alpine:arm .
-docker push derkades/rclone-alpine:arm
--- build.sh
+++ build.sh
@@ -1,2 +1,1 @@
 docker build -t derkades/rclone-alpine:latest .
-docker push derkades/rclone-alpine:latest
